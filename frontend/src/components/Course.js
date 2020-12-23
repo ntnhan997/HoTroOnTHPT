0b8e1b4b6bd69fbd51d13e52069ebe7858d7ca89
ntnhan997 <ntnhan997@gmail.com>
2020-12-23 21:32:09 +0700

    create component Course and Css

---
+++ frontend/src/components/Course.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+
+const Course = () => {
+    return (
+    <div className="course">
+        <div>
+            Course
+        </div>
+    </div>
+    )
+}
+
+export default Course;
